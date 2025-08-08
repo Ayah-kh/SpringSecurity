2ca0e3bb8355e1638dd7b1b997ffb6e2c4984d7c
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2025-08-08 16:46:43 +0300

    First Commit

---
+++ src/main/java/com/example/Security/Demo/SecurityDemoSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.Security.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDemoSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityDemoSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Security/Demo/SecurityDemoSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Security.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityDemoSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
